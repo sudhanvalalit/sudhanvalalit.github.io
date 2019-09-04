c30900b2114496cb433b054150012bb08be487d1
sudhanvalalit <sudsid@gmail.com>
2019-09-04 14:48:24 -0400

    Wordpress folder added

---
+++ style.js
@@ -0,0 +1,9 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href="index.html">Home</a></li>"+
+"<li><a href="education.html">Education</a></li>"+
+"</ul>"; 
+  
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Bhishan Poudel <bp959314@ohio.edu. All rights reserved.</p>";
+
